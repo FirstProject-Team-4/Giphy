de334671e01d06d7e8a62d9c6e387920eb20b163
YordanAleksandrov <dani541112@gmail.com>
2024-01-18 15:47:59 +0200

    bug

--- Giphy/template/src/views/favorites-view.js
+++ Giphy/template/src/views/favorites-view.js
@@ -4,7 +4,7 @@ export const toFavoritesView = (favorites) => `
 <div id="movies">
   <h1>Favorite gifs:</h1>
   <div class="content">
-    ${favorites.map(toTrendingItemView).join('\n') || '<p>Add some movies to favorites to see them here.</p>'}
+    ${favorites.map(toTrendingItemView).join('\n') || '<p>Add some gifs to favorites to see them here.</p>'}
   </div>
 </div>
 `;
