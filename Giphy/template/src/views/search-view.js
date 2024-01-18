a68b6c224c641c36500a6a6053af0ffbc5c35ef3
Veselina Mancheva <veselina.mancheva4@gmail.com>
2024-01-18 16:32:57 +0200

    loop btn

--- Giphy/template/src/views/search-view.js
+++ Giphy/template/src/views/search-view.js
@@ -1,10 +1,10 @@
-import { toMovieSimple } from './movie-views.js';
 
-export const toSearchView = (movies, searchTerm) => `
-<div id="movies">
+export const toSearchView = (gif, searchTerm) => `
+<div id="gif-search">
   <h1>Movies found for "${searchTerm}":</h1>
   <div class="content">
     ${movies.map(toMovieSimple).join('\n') || '<p>Add some movies to favorites to see them here.</p>'}
+    <
   </div>
 </div>
 `;
