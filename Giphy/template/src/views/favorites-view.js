15d0269afe701b80019950d52065bb768b9d5c2b
YordanAleksandrov <dani541112@gmail.com>
2024-01-18 15:44:52 +0200

    small fix

--- Giphy/template/src/views/favorites-view.js
+++ Giphy/template/src/views/favorites-view.js
@@ -2,7 +2,7 @@ import { toTrendingItemView } from './trending-view.js';
 
 export const toFavoritesView = (favorites) => `
 <div id="movies">
-  <h1>Favorite movies:</h1>
+  <h1>Favorite gifs:</h1>
   <div class="content">
     ${favorites.map(toTrendingItemView).join('\n') || '<p>Add some movies to favorites to see them here.</p>'}
   </div>
