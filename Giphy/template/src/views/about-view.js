a80556561969c085d5d27802bfc2370de6d356d2
Veselina Mancheva <veselina.mancheva4@gmail.com>
2024-01-19 14:04:38 +0200

    add about view

--- Giphy/template/src/views/about-view.js
+++ Giphy/template/src/views/about-view.js
@@ -3,7 +3,7 @@ export const toAboutView = () => `
   <div class="content">
     <h1>About the app</h1>
     <h2>Authors: Telerik Academy</h2>
-    <h2>Date: 2021</h2>
+    <h2>Date: 2024</h2>
   </div>
 </div>
 `;
--- Giphy/template/src/views/upload-view.js
+++ Giphy/template/src/views/upload-view.js
@@ -1,11 +1,17 @@
-export const toUploadView=()=>{
+export const toUploadView = (uploadValue) => {
     return `
-    <div class=upload>
-    <input type="file" id="fileInput">
-    <button class="uploadGif" >Upload GIF</button>
-    </div>
-    `
-}
+      <div class=upload>
+        <input type="file" id="fileInput" value="${uploadValue || ''}">
+        <button class="uploadGif" >Upload GIF</button>
+      </div>
+    `;
+  };
 
-
-
+// export const toUploadView = () => {
+//     return `
+//     <form id="uploadForm">
+//     <input type="file" id="fileInput" accept="image/gif" />
+//     <button type="button" onclick="uploadGif()">Upload GIF</button>
+// </form>
+//     `;
+//   };
