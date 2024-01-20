b07ecadb90d970b59a3e604cfb60737ba0ad41c8
Veselina Mancheva <veselina.mancheva4@gmail.com>
2024-01-20 16:32:50 +0200

    fullscreen

--- Giphy/template/src/views/trending-view.js
+++ Giphy/template/src/views/trending-view.js
@@ -14,6 +14,7 @@ export const toTrendingItemView=(trendingItem)=>`
 <li>
     <img src="${trendingItem.images.fixed_width.url}" alt="${trendingItem.title}" class='idGif' data='${trendingItem.id}'>
     <span class="favorite-status"> <data-gif-id="${trendingItem.id}">${renderFavoriteStatus(trendingItem.id)}</span>
+    <span class="fullscreen-button" data-gif-url="${trendingItem.images.fixed_width.url}" id='fullscreen'>🔍</span>
 </li>
 `;
 
