b35def8a2cdd32c71047f70c077468457590cade
YordanAleksandrov <dani541112@gmail.com>
2024-01-21 17:03:59 +0200

    trending bugged css

--- Giphy/template/src/views/trending-view.js
+++ Giphy/template/src/views/trending-view.js
@@ -3,7 +3,7 @@ export const toTrendingView=(trendingGifs)=>{
     return `
     <section class="trending">
     <h2>Trending</h2>
-    <div>
+    <div class='gif-page'>
       ${trendingGifs.map(toTrendingItemView).join('')}
     </div>
   </section>
@@ -11,7 +11,7 @@ export const toTrendingView=(trendingGifs)=>{
 };
 
 export const toTrendingItemView=(trendingItem)=>`
-<div>
+<div class='gif-box'>
     <img src="${trendingItem.images.fixed_width.url}" alt="${trendingItem.title}" class='idGif' data='${trendingItem.id}'>
     <span class="favorite-status"> <data-gif-id="${trendingItem.id}">${renderFavoriteStatus(trendingItem.id)}</span>
     <span class="fullscreen-button" data-gif-url="${trendingItem.images.fixed_width.url}" id='fullscreen'>⛶</span>
