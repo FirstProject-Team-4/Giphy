e5538f661bd70a19e99ab9f5dfd4188b2a0e4d01
YordanAleksandrov <dani541112@gmail.com>
2024-01-21 15:39:30 +0200

    trending style

--- Giphy/template/src/views/trending-view.js
+++ Giphy/template/src/views/trending-view.js
@@ -3,18 +3,18 @@ export const toTrendingView=(trendingGifs)=>{
     return `
     <section class="trending">
     <h2>Trending</h2>
-    <ul>
+    <div>
       ${trendingGifs.map(toTrendingItemView).join('')}
-    </ul>
+    </div>
   </section>
   `;
 };
 
 export const toTrendingItemView=(trendingItem)=>`
-<li>
+<div>
     <img src="${trendingItem.images.fixed_width.url}" alt="${trendingItem.title}" class='idGif' data='${trendingItem.id}'>
     <span class="favorite-status"> <data-gif-id="${trendingItem.id}">${renderFavoriteStatus(trendingItem.id)}</span>
-    <span class="fullscreen-button" data-gif-url="${trendingItem.images.fixed_width.url}" id='fullscreen'>🔍</span>
-</li>
+    <span class="fullscreen-button" data-gif-url="${trendingItem.images.fixed_width.url}" id='fullscreen'>⛶</span>
+</div>
 `;
 
