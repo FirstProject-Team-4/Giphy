88ad204c189355e435ec3f662580f684266d0877
Veselina Mancheva <veselina.mancheva4@gmail.com>
2024-01-21 16:03:42 +0200

    Masonry file

---
+++ Giphy/template/styles/masonry.js
@@ -0,0 +1,9 @@
+// import Masonry from "masonry-layout";
+// var $grid = $(' .trending').imagesLoaded( function() {
+//     $grid.masonry({
+//       itemSelector: '.trending',
+//       percentPosition: true,
+//       columnWidth: '.trending'
+//     }); 
+//   });
+  
