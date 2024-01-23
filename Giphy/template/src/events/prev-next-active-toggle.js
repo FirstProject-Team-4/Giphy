09b292d3e45cb90855c9787759f2156eb82dbe89
YordanAleksandrov <dani541112@gmail.com>
2024-01-23 16:40:07 +0200

    fix-prev/next buttons

--- Giphy/template/src/events/prev-next-active-toggle.js
+++ Giphy/template/src/events/prev-next-active-toggle.js
@@ -1,9 +1,3 @@
 import { q } from "./helpers.js";
 
-export const activeToggle = (page) => {
-const currentActive = q('.activePage');
-if(currentActive){
-  currentActive.classList.remove('activePage');
-}
-page.tail.active.classList.add('activePage');
-}
+
