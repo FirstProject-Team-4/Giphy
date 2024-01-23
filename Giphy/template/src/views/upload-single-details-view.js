da123735f621832df955af1491c42dd150020ccf
Veselina Mancheva <veselina.mancheva4@gmail.com>
2024-01-23 16:55:42 +0200

    fix upload-single view

--- Giphy/template/src/views/upload-single-details-view.js
+++ Giphy/template/src/views/upload-single-details-view.js
@@ -1,16 +1,16 @@
-export const myGifDetailsView=(gifItem)=>{
-    console.log(gifItem)
-    return`
+export const myGifDetailsView = (gifItem) => {
+  return `
   
-
+<div class= 'card'>
   <h1 id='uploadTitle'>Details for Gif</h1>
-  <h1 class='Gif-title'>Title: ${gifItem.myTitle}</h1>
-  
-
-  
+     <div class='Gif-title'>
+  <h1 >Title: ${gifItem.myTitle}</h1>
+        <div id='text'>
   <img src="${gifItem.images.fixed_width.url}" class='myGifUpload'> 
-  <span class='Gif-user'>Name: ${gifItem.myUser}</span>
-  <span class='myGifUpload-date'>Date: ${gifItem.myDate}</span>
-  <span><img src='${gifItem.user.avatar_url}' alt="${gifItem.user.username}" class='Gif-img'></span>
-
+  <p class='Gif-user'>Name: ${gifItem.myUser}</p>
+  <p class='myGifUpload-date'>Date: ${gifItem.myDate}</p>
+  <p><img src='${gifItem.user.avatar_url}' alt="${gifItem.user.username}" class='Gif-img'></p>
+</div>
+</div>
+</div>
   `}
