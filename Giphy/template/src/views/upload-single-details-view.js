c3a48530251fdbd5e1d92ef555058676e9de568b
YordanAleksandrov <dani541112@gmail.com>
2024-01-23 17:02:11 +0200

    upload view in column css

--- Giphy/template/src/views/upload-single-details-view.js
+++ Giphy/template/src/views/upload-single-details-view.js
@@ -1,16 +1,17 @@
 export const myGifDetailsView = (gifItem) => {
   return `
-  
 <div class= 'card'>
   <h1 id='uploadTitle'>Details for Gif</h1>
-     <div class='Gif-title'>
-  <h1 >Title: ${gifItem.myTitle}</h1>
-        <div id='text'>
-  <img src="${gifItem.images.fixed_width.url}" class='myGifUpload'> 
-  <p class='Gif-user'>Name: ${gifItem.myUser}</p>
-  <p class='myGifUpload-date'>Date: ${gifItem.myDate}</p>
-  <p><img src='${gifItem.user.avatar_url}' alt="${gifItem.user.username}" class='Gif-img'></p>
-</div>
-</div>
+  <div class='content-container'>
+    <div class='Gif-title'>
+      <h1>Title: ${gifItem.myTitle}</h1>
+      <img src="${gifItem.images.fixed_width.url}" class='myGifUpload'> 
+    </div>
+    <div id='text'>
+      <p class='Gif-user'>Name: ${gifItem.myUser}</p>
+      <p class='myGifUpload-date'>Date: ${gifItem.myDate}</p>
+      <p><img src='${gifItem.user.avatar_url}' alt="${gifItem.user.username}" class='Gif-img'></p>
+    </div>
+  </div>
 </div>
   `}
