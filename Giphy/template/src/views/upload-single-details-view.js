5411534c39488a6f6f512f04d99910f67ef71bc8
Veselina Mancheva <veselina.mancheva4@gmail.com>
2024-01-23 16:22:06 +0200

    add upload detail view

--- Giphy/template/src/views/upload-single-details-view.js
+++ Giphy/template/src/views/upload-single-details-view.js
@@ -1,17 +1,16 @@
 export const myGifDetailsView=(gifItem)=>{
     console.log(gifItem)
     return`
-  <div class='single-gif-details' >
-  <li>
-  <h1>${gifItem.myTitle}
-  </h1>
+  
 
-  </a>
-  <p>${gifItem.myUser}</p>
-  <p>${gifItem.myData}</p>
-  </li>
-  </div>
-  <div class='description'>
-  <img src='${gifItem.user.avatar_url}' alt="${gifItem.user.username}">
-  </div>
+  <h1 id='uploadTitle'>Details for Gif</h1>
+  <h1 class='Gif-title'>Title: ${gifItem.myTitle}</h1>
+  
+
+  
+  <img src="${gifItem.images.fixed_width.url}" class='myGifUpload'> 
+  <span class='Gif-user'>Name: ${gifItem.myUser}</span>
+  <span class='myGifUpload-date'>Date: ${gifItem.myDate}</span>
+  <span><img src='${gifItem.user.avatar_url}' alt="${gifItem.user.username}" class='Gif-img'></span>
+
   `}
